1295238fbc6811f7fc64111d7beb54253314b9e8
tcongg <tcongg@gmail.com>
2018-06-04 23:33:51 +0700

    [Java] Updated Java Output Formatting

--- Java/Introduction/JavaOutputFormatting.java
+++ Java/Introduction/JavaOutputFormatting.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-            Scanner sc=new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+        
             System.out.println("================================");
-            for(int i=0;i<3;i++){
-                String s1=sc.next();
-                int x=sc.nextInt();
-                System.out.printf("%-15s%03d\n", s1, x);
+        
+            for (int i = 0; i < 3; i++) {
+                String string = scanner.next();
+                int number = scanner.nextInt();
+                System.out.printf("%-15s%03d\n", string, number);
             }
+        
             System.out.println("================================");
 
     }
