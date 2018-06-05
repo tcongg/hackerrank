92d14b83796aa67190bed81f8814a51a6d7ec870
tcongg <tcongg@gmail.com>
2018-06-05 23:47:55 +0700

    [Java] Updated Java Loops II

--- Java/Introduction/JavaLoopsII.java
+++ Java/Introduction/JavaLoopsII.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Solution {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         int q = scanner.nextInt();
-        
+
         for (int k = 0; k < q; k++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             int n = scanner.nextInt();
-            
-            for ( int i = 1; i <= n; i++) {
+
+            for (int i = 1; i <= n; i++) {
                 int s = a;
-                
-                for (int j = 0; j < i; j++){
-                    s = s + (int)Math.pow(2, j) * b;
+
+                for (int j = 0; j < i; j++) {
+                    s = s + (int) Math.pow(2, j) * b;
                 }
-                
+
                 System.out.printf("%d ", s);
             }
-            
+
             System.out.println();
         }
     }
