2e61a4a1d46396aefbeed7e946d39c6fd245f4db
tcongg <39913558+tcongg@users.noreply.github.com>
2018-06-27 15:11:51 +0700

    Updated JavaAnagrams.java

--- Java/String/JavaAnagrams.java
+++ Java/String/JavaAnagrams.java
@@ -29,7 +29,7 @@ public class Solution {
         char tmp;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
-                if (((int)array[j] > (int)array[j + 1]) {
+                if ((int)array[j] > (int)array[j + 1]) {
                     tmp = array[j];
                     array[j] = array[j + 1];
                     array[j +1] = tmp;
